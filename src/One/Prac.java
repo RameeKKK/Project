07c442d5e8eb57f853c194d29b0a925a722106fd
RameeKKK <rameelaskamaraj@gmail.com>
2021-01-18 10:07:01 +0530

    update works

---
+++ src/One/Prac.java
@@ -0,0 +1,16 @@
+package One;
+
+public class Prac {
+public void courseDetails() {
+	System.out.println("c");
+	
+}
+public void empDetail() {
+System.out.println("e");
+}
+public static void main(String[] args) {
+	Prac a=new Prac();
+			a.courseDetails();
+	a.empDetail();
+}
+}
